1eff13003a292aca29973a2a432a4e5243daa747
Daniel Paiva <daniel.p@rushdigital.co.nz>
2025-07-31 09:08:37 +1200

    test: implement comprehensive extension tests
    - Replace placeholder tests with proper TDD structure
    - Add sinon sandbox for clean mocking
    - Test command registration in activate function
    - Test command handlers show expected messages
    - Follow TDD principles with thorough coverage

--- test/unit/extension.test.ts
+++ test/unit/extension.test.ts
@@ -1,11 +1,110 @@
 import * as assert from 'assert';
 import * as vscode from 'vscode';
+import * as sinon from 'sinon';
 
 suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
+    let sandbox: sinon.SinonSandbox;
 
-    test('Sample test', () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    setup(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    teardown(() => {
+        sandbox.restore();
+    });
+
+    suite('Extension Activation', () => {
+        test('should register zoom in command', async () => {
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            assert(registerCommandStub.calledWith('codezoom.zoomIn'));
+            assert.strictEqual(context.subscriptions.length, 3);
+        });
+
+        test('should register zoom out command', async () => {
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            assert(registerCommandStub.calledWith('codezoom.zoomOut'));
+        });
+
+        test('should register reset command', async () => {
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            assert(registerCommandStub.calledWith('codezoom.reset'));
+        });
+    });
+
+    suite('Command Handlers', () => {
+        test('zoom in command should show placeholder message', async () => {
+            const showInfoStub = sandbox.stub(vscode.window, 'showInformationMessage');
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            const zoomInHandler = registerCommandStub.getCall(0).args[1];
+            zoomInHandler();
+
+            assert(showInfoStub.calledWith('CodeZoom: Zoom In (not implemented yet)'));
+        });
+
+        test('zoom out command should show placeholder message', async () => {
+            const showInfoStub = sandbox.stub(vscode.window, 'showInformationMessage');
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            const zoomOutHandler = registerCommandStub.getCall(1).args[1];
+            zoomOutHandler();
+
+            assert(showInfoStub.calledWith('CodeZoom: Zoom Out (not implemented yet)'));
+        });
+
+        test('reset command should show placeholder message', async () => {
+            const showInfoStub = sandbox.stub(vscode.window, 'showInformationMessage');
+            const registerCommandStub = sandbox.stub(vscode.commands, 'registerCommand');
+            
+            const { activate } = await import('../../src/extension');
+            const context = {
+                subscriptions: []
+            } as any;
+
+            activate(context);
+
+            const resetHandler = registerCommandStub.getCall(2).args[1];
+            resetHandler();
+
+            assert(showInfoStub.calledWith('CodeZoom: Reset Zoom (not implemented yet)'));
+        });
     });
 });
